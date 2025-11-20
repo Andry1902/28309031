35e3a06d166503fed9f343177c8354175e7adf55
Andry1902 <andreinacarolaineve@gmail.com>
2025-11-20 17:00:25 -0400

    listado de estudiantes dinámico usando javascript

---
+++ js/index.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    setTimeout(function() {
+        iniciarAplicacion();
+    }, 100);
+});
+
+function iniciarAplicacion() {
+    cargarEstudiantes();
+}
+
+function cargarEstudiantes() {
+    
+    const contenedor = document.getElementById('contenedor-estudiantes');
+    contenedor.innerHTML = '';
+    
+    perfiles.forEach(perfil => {
+        const estudianteDiv = document.createElement('div');
+        estudianteDiv.className = 'caja-estudiante';
+        estudianteDiv.id = `student-${perfil.ci}`;
+        
+        // Andreina con responsive
+        if (perfil.ci === '28309031') {
+            estudianteDiv.innerHTML = `
+                <a href="${perfil.ci}/perfil.html">
+                    <img src="${perfil.ci}/${perfil.ci}Pequena.jpg" alt="${perfil.nombre}" class="foto-estudiante foto-pequena-responsive">
+                    <img src="${perfil.ci}/${perfil.ci}Grande.jpg" alt="${perfil.nombre}" class="foto-estudiante foto-grande-responsive">
+                    <span class="nombre-estudiante">${perfil.nombre}</span>
+                </a>
+            `;
+        } else {
+            estudianteDiv.innerHTML = `<img src="${perfil.imagen}" alt="${perfil.nombre}" class="foto-estudiante" onerror="this.src='dummies/dummy1/dummy.jpg'"> <span class="nombre-estudiante">${perfil.nombre}</span>`;
+        }
+        
+        contenedor.appendChild(estudianteDiv);
+    });
+    
+}
